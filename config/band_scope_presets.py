975cd475c1d6649be93db20b4c4e0fa49e3d834e
Mark J Hughes <mjhughes@hughesmark.com>
2025-06-05 13:45:38 -0700

    Add band scope presets and preset support

---
+++ config/band_scope_presets.py
@@ -0,0 +1,7 @@
+"""Preset parameter mappings for the band scope command."""
+
+BAND_SCOPE_PRESETS = {
+    # Center frequency, step size, span, max hold flag
+    "air": ("00125000", 833, "20M", 0),
+    "race": ("00460000", 1250, "10M", 0),
+}
