51ece04a0a920b118751cbe479ce701487c98124
Mark J Hughes <mjhughes@hughesmark.com>
2025-06-07 20:59:29 -0700

    Extend other commands in help

--- utilities/command/help_utils.py
+++ utilities/command/help_utils.py
@@ -96,7 +96,15 @@ def show_help(commands, command_help, command="", adapter=None):
             "band sweep",
             "custom search",
         ],
-        "Other": ["help", "switch", "exit"],
+        "Other": [
+            "help",
+            "list",
+            "connect",
+            "use",
+            "close",
+            "switch",
+            "exit",
+        ],
     }
 
     # Check if any high-level commands exist in COMMANDS
@@ -141,7 +149,11 @@ def show_help(commands, command_help, command="", adapter=None):
                     or cmd.startswith("custom search")
                 )
             ],
-            "Other": ["help", "switch", "exit"],
+            "Other": [
+                cmd
+                for cmd in sorted(commands)
+                if cmd in standard_commands["Other"]
+            ],
         }
 
         # Ensure Controlling Scanner commands are always displayed if they exist
