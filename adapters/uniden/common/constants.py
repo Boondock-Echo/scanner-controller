33f4214838b7cd962e97290891e3bf3c67ba8851
Mark J Hughes <mjhughes@hughesmark.com>
2025-07-31 18:45:44 -0700

    Use shared constants for frequency calculations

--- adapters/uniden/common/constants.py
+++ adapters/uniden/common/constants.py
@@ -1,9 +1,12 @@
 """Common constants for Uniden scanner adapters."""
 
+# Number of Hertz in one megahertz
+HZ_PER_MHZ = 1_000_000
+
 # Number of Hertz represented by a single scanner unit
 HZ_PER_SCANNER_UNIT = 100
 
 # Number of scanner units in one megahertz
-SCANNER_UNITS_PER_MHZ = 1_000_000 // HZ_PER_SCANNER_UNIT
+SCANNER_UNITS_PER_MHZ = HZ_PER_MHZ // HZ_PER_SCANNER_UNIT
 
-__all__ = ["HZ_PER_SCANNER_UNIT", "SCANNER_UNITS_PER_MHZ"]
+__all__ = ["HZ_PER_MHZ", "HZ_PER_SCANNER_UNIT", "SCANNER_UNITS_PER_MHZ"]
