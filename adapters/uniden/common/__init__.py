6a9e0528d975a4fd01d0f6b6c85985db452a220c
Mark J Hughes <mjhughes@hughesmark.com>
2025-07-31 18:29:09 -0700

    Define common scanner unit constants

--- adapters/uniden/common/__init__.py
+++ adapters/uniden/common/__init__.py
@@ -1,1 +1,5 @@
 """Common functions for Uniden scanner adapters."""
+
+from .constants import HZ_PER_SCANNER_UNIT, SCANNER_UNITS_PER_MHZ
+
+__all__ = ["HZ_PER_SCANNER_UNIT", "SCANNER_UNITS_PER_MHZ"]
